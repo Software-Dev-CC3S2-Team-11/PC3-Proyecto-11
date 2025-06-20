8836446e705fd40ecf32f852984cc82bb2390d6e
amirmiir <00aamircg@gmail.com>
2025-06-19 23:01:53 -0500

    fix: version for testing

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ load_dotenv()
 
 SESSION_SECRET = getenv('SESSION_SECRET')
 
-VERSION = "-1.1.0"
+VERSION = "-1.0.1"
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET)
